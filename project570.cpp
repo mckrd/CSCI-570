67ad3eb0fb4329ad2d35e07f7ba8f7c09dbfd990
Ashley Yu-Chih <oceanicwaves22@gmail.com>
2021-12-09 22:28:11 -0800

    Changed .cpp file to make it compatible with Linux machines and deleted old comments.

--- project570.cpp
+++ project570.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <algorithm>
 #include <string>
-#include <math.h>
-#include <ctype.h>
-#include <ctime>
-#include <cstdlib>
 #include <vector>
-#include <Windows.h>
-#include <Psapi.h>
+
+#include <math.h>
+#include <time.h>
+#include <cstdlib>
+#include <ctype.h>
+
+//Windows Only h files. Comment these out to run on Linux/Mac
+//#include <Windows.h>
+//#include <Psapi.h>
 
 using namespace std;
 
@@ -19,22 +24,12 @@ bool validateLength(string finalString, string baseString, int n)
 
 bool isNumber(string s)
 {
-	for (int i = 0; i < s.length(); i++)
-		if (isdigit(s[i]) == false)
-			return false;
-
-	return true;
+	if (isdigit(s[0]))
+	{
+		return true;
+	}
+	else return false;
 }
-
-//int transform(char c)
-//{
-//	int i = -1;
-//	if (c == 'A') i = 0;
-//	if (c == 'C') i = 1;
-//	if (c == 'G') i = 2;
-//	if (c == 'T') i = 3;
-//	return i;
-//}
 
 const size_t alphabets = 26;
 
@@ -59,8 +54,8 @@ int align(string finalString1, string finalString2, int gap_penalty,
 		{
 			char x_i = finalString1[i - 1];
 			char y_j = finalString2[j - 1];
-			A[i][j] = min( A[i - 1][j - 1] + mismatchPenalty[x_i - 'A'][y_j - 'A'],
-						  min(A[i - 1][j] + gap_penalty, A[i][j - 1] + gap_penalty) );
+			A[i][j] = min(A[i - 1][j - 1] + mismatchPenalty[x_i - 'A'][y_j - 'A'],
+				min(A[i - 1][j] + gap_penalty, A[i][j - 1] + gap_penalty));
 		}
 	}
 
@@ -116,131 +111,7 @@ int align(string finalString1, string finalString2, int gap_penalty,
 	return A[n][m];
 }
 
-//int sequence_alignment(string a1, string b1, int delta, int alpha[4][4], string& a2, string& b2, int flag)
-//{
-//	int m = a1.size();
-//	int n = b1.size();
-//
-//	if (flag == 0) 
-//	{
-//		// m * 2 array
-//		vector<vector<int>> OPT(m + 1, vector<int>(2, 0));
-//
-//		/*
-//			Base case:
-//			OPT[i][0] = i * delta for i = 0 to m
-//		*/
-//		for (int i = 0; i <= m; ++i)
-//			OPT[i][0] = i * delta;
-//
-//		/*
-//			Recurrence:
-//		*/
-//		for (int j = 1; j <= n; ++j) 
-//		{
-//			OPT[0][1] = j * delta;
-//			for(int i = 1; i <= m; ++i)
-//			{
-//				int x_i = transform(a1[i - 1]);
-//				int y_j = transform(b1[j - 1]);
-//				OPT[i][1] = min(OPT[i - 1][0] + alpha[x_i][y_j],
-//					min(OPT[i - 1][1] + delta, OPT[i][0] + delta));
-//			}
-//			for (int i = 0; i <= m; ++i)
-//				OPT[i][0] = OPT[i][1];
-//		}
-//
-//		/*
-//			Back-trace:
-//			NEEDS TO BE FINISHED!!!!!!!
-//		*/
-//
-//		return OPT[m][1];
-//	}
-//
-//
-//	else 
-//	{
-//		// m * n array
-//		vector<vector<int>> OPT(m + 1, vector<int>(n + 1, 0));
-//
-//		/*
-//			Base case:
-//			OPT[0][j] = j * delta for j = 0 to n
-//			OPT[i][0] = i * delta for i = 0 to m
-//		*/
-//		for (int j = 0; j <= n; ++j)
-//			OPT[0][j] = j * delta;
-//		for (int i = 0; i <= m; ++i)
-//			OPT[i][0] = i * delta;
-//
-//		/*
-//			Recurrence:
-//		*/
-//		for (int j = 1; j <= n; ++j)
-//		{
-//			for (int i = 1; i <= m; ++i)
-//			{
-//				int x_i = transform(a1[i - 1]);
-//				int y_j = transform(b1[j - 1]);
-//
-//				OPT[i][j] = min(OPT[i - 1][j - 1] + alpha[x_i][y_j],
-//					min(OPT[i - 1][j] + delta, OPT[i][j - 1] + delta));
-//			}
-//		}
-//
-//		/*
-//			Back-trace:
-//			NOTE THIS IS WRONG!!!!!!!
-//		*/
-//		int i = m;
-//		int j = n;
-//		while (i >= 1 && j >= 1)
-//		{
-//			int x_i = transform(a1[i - 1]);
-//			int y_j = transform(b1[j - 1]);
-//
-//			if (OPT[i][j] == OPT[i - 1][j - 1] + alpha[x_i][y_j])
-//			{
-//				a2 = a1[i - 1] + a2;
-//				b2 = b1[j - 1] + b2;
-//				i--;
-//				j--;
-//			}
-//
-//			else if (OPT[i][j] == OPT[i - 1][j] + delta) //up
-//			{
-//				a2 = a1[i - 1] + a2;
-//				b2 = '-' + b2;
-//				i--;
-//			}
-//			else if (OPT[i][j] == OPT[i][j - 1] + delta) // left
-//			{
-//				a2 = '-' + a2;
-//				b2 = b1[j - 1] + b2;
-//				j--;
-//			}
-//		}
-//
-//		while (i >= 1 && j < 1)
-//		{
-//			a2 = a1[i - 1] + a2;
-//			b2 = '-' + b2;
-//			--i;
-//		}
-//		while (j >= 1 && i < 1)
-//		{
-//			a2 = '-' + a2;
-//			b2 = b1[j - 1] + b2;
-//			--j;
-//		}
-//		return OPT[m][n];
-//	}
-//
-//	return -1;
-//}
-
-void memEff_sequence_alignment(vector<int> &cost, string a1, string b1, int delta, int mismatchPenalty[alphabets][alphabets]) {
+void memEff_sequence_alignment(vector<int>& cost, string a1, string b1, int delta, int mismatchPenalty[alphabets][alphabets]) {
 	int m = a1.size();
 	int n = b1.size();
 
@@ -264,12 +135,12 @@ void memEff_sequence_alignment(vector<int> &cost, string a1, string b1, int delt
 		{
 			char x_i = a1[i - 1];
 			char y_j = b1[j - 1];
-			OPT[i][1] = min( OPT[i - 1][0] + mismatchPenalty[x_i - 'A'][y_j - 'A'],
-						  min(OPT[i - 1][1] + delta, OPT[i][0] + delta) );
+			OPT[i][1] = min(OPT[i - 1][0] + mismatchPenalty[x_i - 'A'][y_j - 'A'],
+				min(OPT[i - 1][1] + delta, OPT[i][0] + delta));
 
 			/*int x_i = transform(a1[i - 1]);
 			int y_j = transform(b1[j - 1]);
-			OPT[i][1] = min(OPT[i - 1][0] + alpha[x_i][y_j], 
+			OPT[i][1] = min(OPT[i - 1][0] + alpha[x_i][y_j],
 							min(OPT[i - 1][1] + delta, OPT[i][0] + delta));*/
 		}
 		for (int i = 0; i <= m; ++i)
@@ -285,17 +156,17 @@ void memEff_sequence_alignment(vector<int> &cost, string a1, string b1, int delt
 }
 
 void D_and_C_Alignment(string a1, string b1, int delta, int mismatchPenalty[alphabets][alphabets],
-						string & a2, string & b2, int & penalty) {
+	string& a2, string& b2, int& penalty) {
 	int m = a1.size();
 	int n = b1.size();
 	int nMid = n / 2;
 	int mMid = 0;
 
-	if (m <= 2 || n <= 2) penalty += align(a1, b1, delta, mismatchPenalty, a2, b2); 
+	if (m <= 2 || n <= 2) penalty += align(a1, b1, delta, mismatchPenalty, a2, b2);
 	else {
 		string bLowerHalf = b1.substr(0, nMid);
 		string bUpperHalf = b1.substr(nMid);
-		vector<int> tcost(a1.size() +1), lcost(a1.size() +1), rcost(a1.size() +1);
+		vector<int> tcost(a1.size() + 1), lcost(a1.size() + 1), rcost(a1.size() + 1);
 
 		string a1Rev = a1;
 		string bUpperHalfRev = bUpperHalf;
@@ -325,14 +196,14 @@ void D_and_C_Alignment(string a1, string b1, int delta, int mismatchPenalty[alph
 		D_and_C_Alignment(aLowerHalf, bLowerHalf, delta, mismatchPenalty, a2, b2, penalty);
 
 
-		
+
 	}
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	//PMC is used to detect how much RAM is used. May or may not be a windows-only command.
-	PROCESS_MEMORY_COUNTERS pmc;
+	//PROCESS_MEMORY_COUNTERS pmc;
 
 	clock_t startTime = clock();
 
@@ -343,8 +214,8 @@ int main(int argc, char *argv[])
 	else
 		cout << "No input file provided \n";
 
-	string baseString1 = "", baseString2 = ""; // The base string in the input file
-	string finalString1 = "", finalString2 = ""; // The final strings generated from the base strings
+	string baseString1, baseString2; // The base string in the input file
+	string finalString1, finalString2; // The final strings generated from the base strings
 
 	int j = 0, k = 0;
 	int counter = 0;
@@ -358,20 +229,24 @@ int main(int argc, char *argv[])
 	if (myFile.is_open())
 	{
 		string line;
+		string command;
 		while (getline(myFile, line))
 		{
+			istringstream iss(line);
+			iss >> command;
+
 			if (counter == 0)
 			{
-				baseString1 += line;
-				finalString1 += line;
+				baseString1 += command;
+				finalString1 += command;
 				counter++;
 			}
 
 			else if (counter == 1)
 			{
-				if (isNumber(line))
+				if (isNumber(command))
 				{
-					int indexToAppend = stoi(line);
+					int indexToAppend = stoi(command);
 					indexToAppend++;
 					finalString1.insert(indexToAppend, finalString1);
 					j++;
@@ -381,57 +256,34 @@ int main(int argc, char *argv[])
 
 			if (counter == 2)
 			{
-				baseString2 += line;
-				finalString2 += line;
+				baseString2 += command;
+				finalString2 += command;
 				counter++;
 			}
 
 			else if (counter == 3)
 			{
-				if (isNumber(line))
+				if (isNumber(command))
 				{
-					int indexToAppend = stoi(line);
+					int indexToAppend = stoi(command);
 					indexToAppend++;
 					finalString2.insert(indexToAppend, finalString2);
 					k++;
+
 				}
 			}
 
 		}
 		myFile.close();
 	}
+
+
 	int m, n;
 	n = validateLength(finalString1, baseString1, j); // Validate 1st generated string is of length (2^j)*str1.length 
 	m = validateLength(finalString2, baseString2, k); // Validate 2nd generated string is of length (2^k)*str2.length
 
 	if (n == -1 || m == -1)
 		cout << "Error in string generation \n";
-
-	// The description asks us to validate the length of the generated strings. Just in case, the function returns false, what do we do?
-	/*
-	cout<<"base string 1 is: "<<a<<"\n";
-	cout<<"j is: "<<j<<"\n";
-	cout<<"final generated string 1 is: "<<a1<<"\n";
-	cout<<"base string 2 is: "<<b<<"\n";
-	cout<<"k is: "<<k<<"\n";
-	cout<<"final generated string 2 is: "<<b1<<"\n";
-	*/
-	/*int alpha[4][4];
-	for (int i = 0; i < 4; ++i)
-		for (size_t j = 0; j < 4; ++j)
-			if (i == j) alpha[i][j] = 0;
-	alpha[0][1] = 110;
-	alpha[0][2] = 48;
-	alpha[0][3] = 94;
-	alpha[1][0] = 110;
-	alpha[1][2] = 118;
-	alpha[1][3] = 48;
-	alpha[2][0] = 48;
-	alpha[2][1] = 118;
-	alpha[2][3] = 110;
-	alpha[3][0] = 94;
-	alpha[3][1] = 48;
-	alpha[3][2] = 110;*/
 
 	/*
 	The mismatch penalty (alpha) table
@@ -472,11 +324,6 @@ int main(int argc, char *argv[])
 	int delta = 30;
 
 	/*
-		The flag to choose basic/memory-efficient version of the algorithm
-	*/
-	int flag = 0;
-
-	/*
 		The resulting aligned strings
 	*/
 	string a2, b2 = "";
@@ -487,17 +334,18 @@ int main(int argc, char *argv[])
 	//penalty = align(finalString1, finalString2, delta, mismatchPenalty, a2, b2);
 
 	//Outputs RAM being used. This code is specific to Windows Machines:
-	bool result = K32GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
-	SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
+	//bool result = K32GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+	//SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
 
-	double msTimer = (clock() - startTime) / (double)(CLOCKS_PER_SEC / 1000);
-	
+	clock_t endTime = clock();
+	double msTimer = (double)(endTime - startTime) / CLOCKS_PER_SEC;
+
 	if (outputFile.is_open()) {
 		outputFile << "a: " << finalString1 << endl;
 		outputFile << "b: " << finalString2 << endl;
 		outputFile << "Penalty: " << penalty << endl;
-		outputFile << "Memory Used: " << physMemUsedByMe / 1024 << endl;
-		outputFile << "Seconds Passed: " << msTimer / 1000 << endl << endl;
+		//outputFile << "Memory Used: " << physMemUsedByMe / 1024 << endl;
+		outputFile << "Seconds Passed: " << msTimer << endl << endl;
 		outputFile << "Aligned sequences: " << endl;
 
 		if (a2.size() <= 50 && b2.size() <= 50) {
@@ -523,12 +371,12 @@ int main(int argc, char *argv[])
 
 			outputFile << "First 50: " << afirst50 << endl;
 			outputFile << "First 50: " << bfirst50 << endl;
-			
+
 			outputFile << "Last 50: " << alast50 << endl;
 			outputFile << "Last 50: " << blast50 << endl;
 		}
 	}
-
+	outputFile << "end of file";
 
 	return 0;
 }
